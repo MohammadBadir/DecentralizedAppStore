5cba816523efc96b94bc33066d3aa7a38e751c58
waseemsarsour <waseemsarsour@hotmail.com>
2022-11-13 14:55:30 +0200

    labels on topbar buttons changed

--- AppStore/src/TopBar.js
+++ AppStore/src/TopBar.js
@@ -7,11 +7,11 @@ class TopBar extends React.Component{
         <div className='topBar-background'>
            <div className='topBar'>
               <h1 style={{color: "white",fontFamily: "cursive"}} className='appName'>Decentralized Appstore</h1>
-              <button id='defaultAppsView' className='topButtons' data-index={0} onClick={this.props.changeView}>Apps</button>
+              <button id='defaultAppsView' className='topButtons' data-index={0} onClick={this.props.changeView}>Browse</button>
               <button className='topButtons' data-index={1} onClick={this.props.changeView}>My Apps</button>
               <button className='topButtons' data-index={2} onClick={this.props.changeView}>Purchases</button>
-              <button className='topButtons' data-index={3} onClick={this.props.changeView}>Upload App</button>
-              <span  style={{fontSize:"18px"}}  id="accountNumber">{this.props.userName}</span>
+              <button className='topButtons' data-index={3} onClick={this.props.changeView}>Publish App</button>
+              <span  style={{fontSize:"18px"}}  id="accountNumber">Logged in as: {this.props.userName}</span>
            </div>
         </div>
          );
